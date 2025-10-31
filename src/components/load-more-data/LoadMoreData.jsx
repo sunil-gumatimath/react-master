d62d41d8b893cbbb1d0f741fedd9581580a9e411
SuniL <sunilgumatimath.vs@gmail.com>
2025-10-31 18:57:24 +0530

    feat(components): Add LoadMoreData component
    Introduce the `LoadMoreData` React component and its associated CSS file.
    This component is set up with initial state variables for managing loading status, product data, and a count, laying the groundwork for implementing a "load more" data feature.

---
+++ src/components/load-more-data/LoadMoreData.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useState } from 'react'
+
+const LoadMoreData = () => {
+
+
+  const [loading, setLoading] = useState(false);
+  const [product, setProduct] = useState([])
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className='container'>
+      
+    </div>
+  )
+}
+
+export default LoadMoreData
