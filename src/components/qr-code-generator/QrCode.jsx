e6a554c7e9059a6df1442632bbc318c047ceb0b9
SuniL <sunilgumatimath.vs@gmail.com>
2025-11-01 19:53:59 +0530

    feat: add QR code generation input and display
    - Import QRCode component from react-qr-code
    - Add input field and generate button for user text input
    - Render QRCode component to display generated QR code based on input

--- src/components/qr-code-generator/QrCode.jsx
+++ src/components/qr-code-generator/QrCode.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import QRCode from 'react-qr-code'
 
 const QrCode = () => {
+
+
+
+    
   return (
     <div>
         <h1>Qr Code Generator </h1>
         
+        <div>
+            <input type="text" name='qr-code' placeholder='Enter your value here'/>
+            <button>Generate</button>
+        </div>
 
+        <div>
+            <QRCode
+            id='qr-code-value'
+            />
+        </div>
     </div>
   )
 }
