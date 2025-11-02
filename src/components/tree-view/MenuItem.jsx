029769fdd555fc600cff7ea40659ec8f504c32e2
SuniL <sunilgumatimath.vs@gmail.com>
2025-11-02 18:30:47 +0530

    feat(components/tree-view): replace text toggle with icons in MenuItem
    - Import FaMinus and FaPlus icons from react-icons/fa
    - Replace "+" text with FaPlus/FaMinus icons for expand/collapse functionality
    - Add condition to display MenuList only when item is expanded, enhancing UI clarity and visual appeal

--- src/components/tree-view/MenuItem.jsx
+++ src/components/tree-view/MenuItem.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import MenuList from './MenuList'
-
+import {FaMinus,FaPlus} from 'react-icons/fa'
 const MenuItem = ({ item }) => {
 
   const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
@@ -18,12 +18,16 @@ const MenuItem = ({ item }) => {
       <div style={{ display: "flex", gap: '20px' }}>
         <p>{item.label}</p>
         {
-          item && item.children && item.children.length ? <span onClick={()=>handleToggleChildren(item.label)}>+</span> : null
+          item && item.children && item.children.length ? <span onClick={()=>handleToggleChildren(item.label)}>
+            {
+              displayCurrentChildren[item.label] ? <FaMinus color='#fff' size={25}/> : <FaPlus color='#fff' size={25}/>
+            }
+          </span> : null
         }
       </div>
 
       {
-        item && item.children && item.children.length > 0 ?
+        item && item.children && item.children.length > 0 && displayCurrentChildren[item.label] ?
           <MenuList list={item.children} />
           : null
       }
